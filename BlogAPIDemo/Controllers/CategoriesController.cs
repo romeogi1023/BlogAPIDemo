f19594c138dbc0f4590c0159effa4e73a68951a5
eddie <bda605@gmail.com>
2020-03-17 18:00:20 +0800

    Controller 追加CategoryService 撰寫

--- BlogAPIDemo/Controllers/CategoriesController.cs
+++ BlogAPIDemo/Controllers/CategoriesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlogAPIDemo.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogAPIDemo.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
